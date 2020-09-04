2edd93f81446425e00b06f9f8e328c343da6db00
Shyaboi <ian.shane.sears@gmail.com>
2020-09-03 23:48:30 -0700

    muhcors

--- server.js
+++ server.js
@@ -27,9 +27,7 @@ app.use(express.static(__dirname + "./public/"));
 // coooooooooooooooooorrrrrrrrrrrrrrrrrrrrrrrrrssssssssssssssssssssssssssssss
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
 });
 // coooooooooooooooooorrrrrrrrrrrrrrrrrrrrrrrrrssssssssssssssssssssssssssssss
 
